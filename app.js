42d8c235a0df4b5469074e516799fade42ec17e4
Mariana Obrajero <abigailobrajero@gmail.com>
2023-05-11 14:52:00 +0000

    ⭐Add Middleware

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ import express from 'express';
 
 // Crear una instancia de express
 const app = express(); // (req,res) => {UN MONTON DE CÓDIGO}
+app.use((req, res, next) => {
+    console.log("😎 Ejecutando el middleware 1");
+    // Incovando el siguiente Middleware
+    next();
+});
+app.use((req, res, next) => {
+    console.log(`${req.method}-${req.url}`);
+    next();
+});
+app.use((req, res, next) => {
+    console.log("👀 Respondiendo al cliente");
+    res.send(`
+    <h1>Welcome to Express ♥ </h1>
+    <p>This is my awesome app</p>
+    `);
+});
 
 // Creando el servidor
 const server = http.createServer(app);
